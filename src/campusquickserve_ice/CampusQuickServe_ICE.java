2de90b5f917d7fea0b2cfecde869a837ab573a66
CalebP-ST10504188 <ST10504188@myemeris.edu.za>
2026-03-06 15:13:41 +0200

    Constructor, subtotalCalc, vatrateCalc methods
    Implemented a constructor method with JOptionPane dialog inputs.
    
    Implemented subtotalCalc method that multiplies the price per item with the number of items ordered.
    
    Implemented vatrateCalc method that multiplies the subtotal with the constant VAT rate.

--- src/campusquickserve_ice/CampusQuickServe_ICE.java
+++ src/campusquickserve_ice/CampusQuickServe_ICE.java
@@ -3,7 +3,7 @@ package campusquickserve_ice;
 public class CampusQuickServe_ICE {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+      
     }
     
 }
--- src/campusquickserve_ice/Order.java
+++ src/campusquickserve_ice/Order.java
@@ -1,5 +1,38 @@
 package campusquickserve_ice;
 
+import javax.swing.JOptionPane;
+
 public class Order {
+    String customerName, itemOrdered, StudentNumberInput, quantityInput, PriceInput;
+    int studentNum, Quantity;
+    double pricePerItem; 
     
+    //Subtotal Calculation method that multiplies quantity with price per item
+    public static double subtotalCalc(int quant, double priceItem){
+    return quant * priceItem;
+    }
+    
+    public static double vatAddedCalc(double subtotal){
+    final double VAT_RATE = 0.15; //Constant declared for Vat Rate
+    return subtotal * VAT_RATE;
+    }
+    
+    //Constructor
+    public Order(){      
+    customerName = JOptionPane.showInputDialog(null, "Enter your full name:");
+    
+    StudentNumberInput = JOptionPane.showInputDialog(null, "Enter your student number:");
+    studentNum = Integer.parseInt(StudentNumberInput);
+    
+    itemOrdered = JOptionPane.showInputDialog(null, "Enter your order:");
+    
+    quantityInput = JOptionPane.showInputDialog(null, "Enter the number of items ordered:");
+    Quantity = Integer.parseInt(quantityInput);
+    
+    PriceInput = JOptionPane.showInputDialog(null, "Enter the price per item:");
+    pricePerItem = Double.parseDouble(PriceInput);
+    
+    
+    
+    }
 }
